da1624e20cc0de487bd3e6b05a0114e6e0635201
Connor <connortruscott@hotmail.com>
2025-09-25 12:47:25 +1000

    Fix in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
-//app.use('/api/auth', require('./routes/adminRoutes'));
+app.use('/api/auth', require('./routes/adminRoutes'));
 app.use('/api', require('./routes/adminRoutes'));
 app.use('/api/complaints', require('./routes/complaintRoutes'));
 app.use('/api/feedback', require('./routes/feedbackRoutes'));
+
 
 // Export the app object for testing or start server if run directly
 if (require.main === module) {
