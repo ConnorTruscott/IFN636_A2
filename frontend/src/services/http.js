8051ba9ed312e2f8db8058869c3c03ea19d04912
Connor <connortruscott@hotmail.com>
2025-10-02 15:07:34 +1000

    EC2 fixed???????

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const baseURL = process.env.REACT_APP_API_URL || axios.baseURL;
 const http = axios.create({ baseURL });
 http.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
