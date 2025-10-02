4fa0590bab43df9adb3687d98bf823d5f8c1ede8
Connor <connortruscott@hotmail.com>
2025-10-02 22:04:36 +1000

    Fix Broken admin dashboard

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,28 +1,28 @@
 import axiosInstance from "../axiosConfig";
 
-export const adminGetComplaints = async (token, params = {}) => {
+export const adminGetComplaints = async (token, id) => {
   const { data } = await axiosInstance.get('/api/admin/complaints', {
-    params,
+    id,
     headers: { Authorization: `Bearer ${token}` },
   });
   return data;
 };
 
-export const adminGetComplaint = async (id, token) => {
+export const adminGetComplaint = async (token, id) => {
   const { data } = await axiosInstance.get(`/api/admin/complaints/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return data;
 };
 
-export const adminUpdateComplaint = async (id, dataObj, token) => {
+export const adminUpdateComplaint = async (token, id, dataObj) => {
   const { data } = await axiosInstance.put(`/api/admin/complaints/${id}`, dataObj, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return data;
 };
 
-export const adminDeleteComplaint = async (id, reason, token) => {
+export const adminDeleteComplaint = async (token, id, reason) => {
   const { data } = await axiosInstance.delete(`/api/admin/complaints/${id}`, {
     data: { reason },
     headers: { Authorization: `Bearer ${token}` },
