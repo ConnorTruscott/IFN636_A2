9f6f46fa064bbfae7dea91a843eb6d8a6eda6e4c
Connor <connortruscott@hotmail.com>
2025-10-02 19:23:57 +1000

    Still EC2 failing

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.253.4.198:5001', // live
+  baseURL: 'http://13.211.21.105:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
