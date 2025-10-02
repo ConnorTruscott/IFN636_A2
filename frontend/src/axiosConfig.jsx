d01389ac41bd04e064456e7e5efccc22f7dd3669
Connor <connortruscott@hotmail.com>
2025-10-02 19:53:19 +1000

    EC2 finally worked

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.211.21.105:5001', // live
+  baseURL: 'http://3.25.142.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
