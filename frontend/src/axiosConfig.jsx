136e326c7a8ef5e2662e7c792d2f3e20e71211a6
Connor <connortruscott@hotmail.com>
2025-10-02 18:43:41 +1000

    New IP, yet again...

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.26.67.156:5001', // live
+  baseURL: 'http://54.66.159.69:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
